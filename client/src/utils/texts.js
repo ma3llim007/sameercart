2d921edd52f149440f9c3e3e5e53e2a89ccc53d6
ma3llim007 <mohdsameer68257@gmail.com>
2025-02-20 18:03:39 +0530

    added option channing

--- client/src/utils/texts.js
+++ client/src/utils/texts.js
@@ -1,6 +1,6 @@
 export const capitalizeWords = str => {
     return str
-        .trim()
+        ?.trim()
         .split(" ")
         .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
         .join(" ");
