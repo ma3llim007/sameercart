6007a2ddd657cfdf67069ee7617a6992d0ade728
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-03 22:43:42 +0530

    added few secure featured and functionality like (setup helment, express-rate-limit, express-slow-down, compress)

--- server/src/app.js
+++ server/src/app.js
@@ -19,40 +19,52 @@ import publicOrderRouter from "./routes/order.routes.js";
 import publicContactRouter from "./routes/contact.routes.js";
 import publicBlogRouter from "./routes/blog.routes.js";
 import "./config/passportStrategy.js";
+import helmet from "helmet";
+import { authLimiter, publicLimiter, speedLimiter } from "./middlewares/midlewares.js";
+import compression from "compression";
 
 const app = express();
 
 // Middleware
-app.use(
-    cors({
-        origin: process.env.CORS_ORIGIN,
-        credentials: true,
-    })
-);
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.use(helmet());
+app.use(speedLimiter);
+app.use(
+    compression({
+        threshold: 1024,
+        level: 6,
+        filter: (req, res) => {
+            if (req.headers["x-no-compression"]) {
+                return false;
+            }
+            return compression.filter(req, res);
+        },
+    })
+);
 
 // Admin Routes
-app.use("/api/v1/admin/auth", adminAuthRouter);
-app.use("/api/v1/admin/category", categoryRouter);
-app.use("/api/v1/admin/sub-category", subCategoryRouter);
-app.use("/api/v1/admin/product", productRouter);
-app.use("/api/v1/admin/variant", variantRouter);
-app.use("/api/v1/admin/order", orderRouter);
-app.use("/api/v1/admin/users", usersRouter);
-app.use("/api/v1/admin/blog", blogRouter);
-app.use("/api/v1/admin/contact", contactRouter);
+app.use("/api/v1/admin/auth", authLimiter, adminAuthRouter);
+app.use("/api/v1/admin/category", publicLimiter, categoryRouter);
+app.use("/api/v1/admin/sub-category", publicLimiter, subCategoryRouter);
+app.use("/api/v1/admin/product", publicLimiter, productRouter);
+app.use("/api/v1/admin/variant", publicLimiter, variantRouter);
+app.use("/api/v1/admin/order", publicLimiter, orderRouter);
+app.use("/api/v1/admin/users", publicLimiter, usersRouter);
+app.use("/api/v1/admin/blog", publicLimiter, blogRouter);
+app.use("/api/v1/admin/contact", publicLimiter, contactRouter);
 
 // Public Routes
-app.use("/api/v1/category", publicCategoryRouter);
-app.use("/api/v1/sub-category", publicSubCategoryRouter);
-app.use("/api/v1/products", publicProductRouter);
-app.use("/api/v1/users", publicUserRouter);
-app.use("/api/v1/users/dashboard", publicDashboardRouter);
-app.use("/api/v1/order", publicOrderRouter);
-app.use("/api/v1/contacts", publicContactRouter);
-app.use("/api/v1/blog", publicBlogRouter);
+app.use("/api/v1/category", publicLimiter, publicCategoryRouter);
+app.use("/api/v1/sub-category", publicLimiter, publicSubCategoryRouter);
+app.use("/api/v1/products", publicLimiter, publicProductRouter);
+app.use("/api/v1/users", publicLimiter, publicUserRouter);
+app.use("/api/v1/users/dashboard", publicLimiter, publicDashboardRouter);
+app.use("/api/v1/order", publicLimiter, publicOrderRouter);
+app.use("/api/v1/contacts", publicLimiter, publicContactRouter);
+app.use("/api/v1/blog", publicLimiter, publicBlogRouter);
 
 export { app };
---
+++ server/src/middlewares/midlewares.js
@@ -0,0 +1,29 @@
+import rateLimit from "express-rate-limit";
+import slowDown from "express-slow-down";
+import { ApiError } from "../utils/index.js";
+
+// General limiter for public routes
+const publicLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 Minutes
+    max: 100,
+    handler: (_, res) => {
+        return res.status(429).json(new ApiError(429, null, "Too Many Requests Form This IP, Please Try Again Later."));
+    },
+});
+
+// Stricter limiter for authentication routes
+const authLimiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 Minutes
+    max: 10,
+    handler: (_, res) => {
+        return res.status(429).json(new ApiError(429, null, "Too Many Requests Form This Ip, Please Try Again Later."));
+    },
+});
+
+const speedLimiter = slowDown({
+    windowMsL: 15 * 60 * 1000,
+    delayAfter: 50,
+    delayMs: () => 500,
+});
+
+export { publicLimiter, authLimiter, speedLimiter };
