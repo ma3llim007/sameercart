14721a2f1a6d9033eb5b3857ea99f78a98489c15
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-10 21:41:31 +0530

    fix: timeout Request in apiInstance; remove the not required part in ErrorBoundary

--- client/src/api/apiInstance.js
+++ client/src/api/apiInstance.js
@@ -4,7 +4,6 @@ import axios from "axios";
 // Public Axios instance
 const axiosInstancePublic = axios.create({
     baseURL: import.meta.env.VITE_ADMIN_FRONTEND_URL || "http://localhost:8000",
-    timeout: 10000,
     withCredentials: true,
 });
 
--- client/src/components/ErrorBoundary.jsx
+++ client/src/components/ErrorBoundary.jsx
@@ -21,11 +21,8 @@ class ErrorBoundary extends Component {
                 <div className="flex flex-col items-center justify-center min-h-screen px-6 bg-gray-100 dark:bg-gray-900">
                     <div className="max-w-md w-full bg-white dark:bg-gray-800 p-8 rounded-2xl shadow-xl text-center border border-gray-200 dark:border-gray-700">
                         <AlertTriangle className="text-red-500 w-14 h-14 mx-auto animate-bounce" />
-                        <h2 className="text-2xl font-semibold text-gray-900 dark:text-white mt-4">Oops! Something went wrong.</h2>
-                        <p className="text-gray-600 dark:text-gray-300 mt-2">We encountered an error while loading the page. Please try again.</p>
-                        <div className="mt-4 p-4 bg-gray-100 dark:bg-gray-700 rounded-md text-sm text-gray-800 dark:text-gray-200 uppercase">
-                            <strong>Error:</strong> {this.state.errorMessage}
-                        </div>
+                        <h2 className="text-2xl font-semibold text-gray-900 dark:text-white mt-4">Oops! Something Went Wrong.</h2>
+                        <p className="text-gray-600 dark:text-gray-300 mt-2">We Encountered An Error While Loading The Page. Please Try Again.</p>
                         <button className="mt-6 bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-6 rounded-lg transition-transform transform hover:scale-105" onClick={this.handleReset}>
                             Try Again
                         </button>
